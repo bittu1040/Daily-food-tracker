c5ced7b359c46d8dacea12dcad2c0626f463b7e4
Bittu <bittu1040@gmail.com>
2025-01-07 17:05:47 +0530

    added bootstrap and ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbAccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
